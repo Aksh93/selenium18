86e8a6ff775e6e4063ecc980e34f6e648df85042
Akshay Patil <155887780+Aksh93@users.noreply.github.com>
2025-01-31 23:06:43 +0530

    Update LoginDataProvider.java

--- src/test/java/com/ui/dataProviders/LoginDataProvider.java
+++ src/test/java/com/ui/dataProviders/LoginDataProvider.java
@@ -1,4 +1,4 @@
-package com.ui.dataProviders;
+package com.ui.dataproviders;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,30 +10,38 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.google.gson.Gson;
+import com.ui.pojo.TestData;
 import com.ui.pojo.User;
-import com.ui.pojo.testData;
+import com.utility.CSVReaderUtility;
+import com.utility.ExcelReaderUtility;
 
 public class LoginDataProvider {
 
-	@DataProvider(name="LoginTestDataProvider")
-	
-	public Iterator<Object[]> loginDataProvide() throws FileNotFoundException {
+	@DataProvider(name = "LoginTestDataProvider")
+	public Iterator<Object[]> loginDataProvider() throws FileNotFoundException {
+		Gson gson = new Gson();
+		File testDataFile = new File(System.getProperty("user.dir") + "//testData//logindata.json");
+		FileReader fileReader = new FileReader(testDataFile);
+		TestData data = gson.fromJson(fileReader, TestData.class);// deserialization
 
-	Gson gson = new Gson();
-	File testDataFile = new File(System.getProperty("user.dir")+"\\testData\\loginData.json");
-	FileReader fileReader = new FileReader(testDataFile);
-	testData data = gson.fromJson(fileReader, testData.class); 
-	 
-	List<Object[]> dataToReturn = new ArrayList<Object[]>();
-	 
-	 for (User user: data.getData()) {
-		 
-		 dataToReturn.add(new Object[] {user});
-	 }
-		 return dataToReturn.iterator();
-		 
-	 }
-	 
+		List<Object[]> dataToReturn = new ArrayList<Object[]>();
+		for (User user : data.getData()) {
+
+			dataToReturn.add(new Object[] { user });
+		}
+
+		return dataToReturn.iterator();
+
+	}
+
+	@DataProvider(name = "LoginTestCSVDataProvider")
+	public Iterator<User> loginCSVDataProvider() {
+		return CSVReaderUtility.readCSVFile("loginData.csv");
+	}
+
+	@DataProvider(name = "LoginTestExcelDataProvider")
+	public Iterator<User> loginExcelDataProvider() {
+		return ExcelReaderUtility.readExcelFile("LoginData.xlsx");
+	}
+
 }
-
-
