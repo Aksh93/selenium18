7b04cf25aa5e8edf831cae7c4ec2364d545a0906
Akshay Patil <155887780+Aksh93@users.noreply.github.com>
2025-01-31 23:18:13 +0530

    Update TestBase.java

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -1,23 +1,57 @@
 package com.ui.tests;
 
-import static com.constants.Browser.EDGE;
-
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
-import com.ui.pages.homePage;
+import com.constants.Browser;
+import com.ui.pages.HomePage;
 import com.utility.BrowserUtility;
+import com.utility.LambdaTestUtility;
+import com.utility.LoggerUtility;
 
 public class TestBase {
-	protected homePage home;
-	
-	@BeforeMethod(description = "load th homepage of website")
-	public void setup() {
-	home = new homePage(EDGE);
-	
-}
-	
+	protected HomePage homePage;
+	Logger logger = LoggerUtility.getLogger(this.getClass());
+	private boolean isLambdaTest;
+
+	@BeforeMethod(description = "Load the Homepage of the website")
+	@Parameters({ "browser", "isLambdaTest", "isHeadless" })
+
+	public void setup(@Optional("chrome") String browser, @Optional("false") boolean isLambdaTest,
+			@Optional("false") boolean isHeadless, ITestResult result) {
+
+		this.isLambdaTest = isLambdaTest;
+		WebDriver lambdaDriver;
+		if (isLambdaTest) {
+
+			lambdaDriver = LambdaTestUtility.intializeLambdaTestSession("chrome", result.getMethod().getMethodName());
+			homePage = new HomePage(lambdaDriver);
+
+		} else {
+			// Running the test on local machine!!!
+			logger.info("Load the Homepage of the website");
+			homePage = new HomePage(Browser.valueOf("chrome".toUpperCase()), isHeadless);
+
+		}
+	}
+
 	public BrowserUtility getInstance() {
-	
-		return home;
-}
+		return homePage;
+	}
+
+	@AfterMethod(description = "Tear Down the browser")
+	public void tearDown() {
+
+		if (isLambdaTest) {
+			LambdaTestUtility.quitSession(); // quit or close the browsersession on LT
+		} else {
+			homePage.quit(); // local
+		}
+	}
+
 }
