b2b3ed55fccd49710de73dafc1595e1b373eae7f
Akshay Patil <155887780+Aksh93@users.noreply.github.com>
2025-01-31 23:17:36 +0530

    Update LoginTest.java

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -1,33 +1,56 @@
 package com.ui.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import static com.constants.Browser.*;
 
-import com.utility.BrowserUtility;
+import static org.testng.Assert.*;
 
-public class LoginTest {
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
-		WebDriver wd = new ChromeDriver(); //launch the application 
-		
-		BrowserUtility browserUtility = new BrowserUtility(wd);
-		browserUtility.goToWebsite("http://www.automationpractice.pl/index.php?controller=authentication&back=my-account");
+import com.ui.pages.HomePage;
+import com.ui.pojo.User;
+import com.utility.LoggerUtility;
 
-		browserUtility.maximizewindow();
+@Listeners({com.ui.listeners.TestListener.class})
+
+public class LoginTest extends TestBase {
+	Logger logger =  LoggerUtility.getLogger(this.getClass());
+
+
+
+	@Test(description = "Verifies if the valid user is able to login into the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
+	public void loginTest(User user) {
+
+		/*
+		 * Test Script has to be small You cannot have conditional statements, loops,
+		 * try-catch in your test method Test Scripts should be only test steps Reduce
+		 * use of local variables Atleast one assertion
+		 */
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Mohit Bhatia");
+
+	}
+
+//	@Test(description = "Verifies if the valid user is able to login into the application", groups = { "e2e",
+//			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+//	public void loginCSVTest(User user) {
+//
+//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+//				"Mohit Bhatia");
+//
+//	}
+
+	@Test(description = "Verifies if the valid user is able to login into the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginExcelTest(User user) {
 		
-		By signInLinkLocator = By.xpath("//a[contains(text(), 'Sign in')]");
-		browserUtility.clickon(signInLinkLocator);
 		
-		
-		browserUtility.enterText(emailTextBoxLocator, "yopije6932@kurbieh.com" );
-		
-		
-		browserUtility.enterText(passTextBoxLocator, "Akshay@1806" );
-		
-		
-		browserUtility.clickon(submitLocator);
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Mohit Bhatia");
 		
 	}
 
